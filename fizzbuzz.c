bd3e259fa19bdd0038b2cf61c81982c39694af93
Martin Dengler <martin@martindengler.com>
2012-03-12 10:27:26 +0800

    make fizzbuzz.c problem-statement-compliant, if uglier

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,21 +2,13 @@
 
 int main(int argc, char ** argv) {
   int i = 1;
-  for (i=1; i <= 100; i++) {
-    printf("%i", i);  /* Yes, I read the problem statement, but the
-                       * output is cleaner this way, so stop slavishly
-                       * doing meaningless programming tasks because
-                       * some guy says most people in your industry
-                       * can't do them ;).
-                       */
+  for (int i=1; i <= 100; i++) {
     if (i % 3 == 0)
-      printf(" Fizz");
-    if (i % 5 == 0) {
-      if (i % 3 != 0)
-        printf(" ");
+      printf("Fizz");
+    if (i % 5 == 0)
       printf("Buzz");
-    }
+    if (! ((i % 5 == 0) || (i % 3 == 0)))
+      printf("%d", i);
     printf("\n");
   }
-
 }
