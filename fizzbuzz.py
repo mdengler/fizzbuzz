f07db7b3846c7302136a0057730797529f191b35
Martin Dengler <martin@martindengler.com>
2013-04-27 02:01:11 +0100

    fizzbuzz.py: Python 3 compatible

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,14 +2,14 @@
 """
 http://c2.com/cgi/wiki?FizzBuzzTest
 """
+import sys
 
 for i in range(1, 101):
-    if i % 5 == 0 and i % 3 == 0:
-        print "%i FizzBuzz" % i
-    else:
-        if i % 5 == 0:
-            print "%i Buzz" % i
-        elif i % 3 == 0:
-            print "%i Fizz" % i
-        else:
-            print i
+    if i % 3 == 0:
+        sys.stdout.write("Fizz")
+    if i % 5 == 0:
+        sys.stdout.write("Buzz")
+    if (i % 3 != 0) and (i % 5 != 0):
+        sys.stdout.write(str(i))
+    sys.stdout.write("\n")
+
